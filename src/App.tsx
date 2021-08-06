6209cff8adce16462679b812656419a84044ca5f
soca-git <soca.mailbox@gmail.com>
2021-08-06 11:29:02 +0100

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let firstValue : string;
+    firstValue = 'Seán';
+    let aNumber : number = 19;
+
+    // two ways to declare an array.
+    // let someNumbers : number[] = [1, 2, 3, 4, 5];
+    let someNumbers : Array<number> = [1, 2, 3, 4, 5];
+
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>This project is using TypeScript!</p>
+          <p>The value {firstValue} is of type {typeof firstValue}!</p>
+          <p>The value of {aNumber} is of type {typeof aNumber}!</p>
+          <p>{someNumbers}</p>
         </header>
       </div>
     );
